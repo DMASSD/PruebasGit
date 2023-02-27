8f6732658388c48faeb80a09a147df0184ea5543
DMASSD <126439616+DMASSD@users.noreply.github.com>
2023-02-26 22:50:53 -0600

    Update EmpresaA.java

--- Empresa A/src/empresa/a/EmpresaA.java
+++ Empresa A/src/empresa/a/EmpresaA.java
@@ -9,7 +9,7 @@ public class EmpresaA {
     public static void main(String[] args) {
 
         Empleados mecanico = new Empleados();
-        Nominas mecanicoNomina = new Nominas();
+        Nominas mecanicoNomina = new Nominas(); //Cambiar el consutructor, este comentario esta hecho desde GitHub jejeje
         
         
         System.out.println(mecanico.nombre);   
