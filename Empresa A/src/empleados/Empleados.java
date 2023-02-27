3e6c3b14d393e1beadd93d2a09bf125766c2d5d9
Daniel Mass Dominguez <danielmassdominguez@gmail.com>
2023-02-26 20:24:03 -0600

    Se crea clase Empleados

--- Empresa A/src/empleados/Empleados.java
+++ Empresa A/src/empleados/Empleados.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package empleados;
 
-/**
- *
- * @author Daniel
- */
 public class Empleados {
+    
+    public String nombre;
+    
+    public int edad;
+    
+    public Empleados(){
+        
+        nombre = "Juanito";
+        
+        edad = 18;
+        
+    }
     
 }
--- Empresa A/src/empresa/a/EmpresaA.java
+++ Empresa A/src/empresa/a/EmpresaA.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package empresa.a;
 
-/**
- *
- * @author Daniel
- */
+import empleados.*;
+
+
 public class EmpresaA {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Empleados mecanico = new Empleados();
+       
+        System.out.println(mecanico.nombre);   
+               
     }
     
 }
